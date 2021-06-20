d9908ad4d7f13ec6629889eb56f28e63b6b158d8
mmmarceloo <83249188+mmmarceloo@users.noreply.github.com>
2021-06-20 00:18:31 -0300

    Add files via upload
    alteracoes e melhorias no slide

--- script.js
+++ script.js
@@ -2,16 +2,21 @@ function slide(a){
    switch(a){
     case 1:
     
-    document.querySelector("#foto").src="image/image1.png";
+    document.querySelector("#foto").src="image/foto8.jpeg";
         break
     case 2:
-        document.querySelector(".banner img").style.left="-200px";    
-        document.querySelector(".banner img").style.left="-200px";   
-        document.querySelector("#foto").src="image/image2.jpg";
+       /* document.querySelector(".banner img").style.left="-200px";    
+        document.querySelector(".banner img").style.left="-200px";   */
+        document.querySelector("#foto").src="image/foto11.jpeg";
         break
-        default:
-        alert('nao é o 1');
-        break    
+    case 3: 
+        document.querySelector("#foto").src="image/foto3.jpeg";   
+        break
+    case 4:
+        document.querySelector("#foto").src="image/foto4.jpeg";
+        break
+ 
+         
 
    }
    
