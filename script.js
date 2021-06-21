5f413438aae607e24db7289bbbff551007815f32
mmmarceloo <83249188+mmmarceloo@users.noreply.github.com>
2021-06-21 20:14:15 -0300

    Add files via upload
    aqui esta a parte de portifolio de vendas, falta fazer o zoom das fotos quando sao clicadas

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
-function slide(a){
-   switch(a){
-    case 1:
-    
-    document.querySelector("#foto").src="image/foto8.jpeg";
-        break
-    case 2:
-       /* document.querySelector(".banner img").style.left="-200px";    
-        document.querySelector(".banner img").style.left="-200px";   */
-        document.querySelector("#foto").src="image/foto11.jpeg";
-        break
-    case 3: 
-        document.querySelector("#foto").src="image/foto3.jpeg";   
-        break
-    case 4:
-        document.querySelector("#foto").src="image/foto4.jpeg";
-        break
- 
-         
+var time = 3000;
+   var i=0;
+   var images = [];
 
+
+   images[0] = "image/foto11.jpeg";
+   images[1] = "image/foto3.jpeg";
+   images[2] = "image/foto4.jpeg";
+   images[3] = "image/foto8.jpeg";
+
+
+
+function slide(a){
+     document.querySelector("#foto").src= images[a];
+        
    }
    
+   
+
+function slideAuto(){
+    document.querySelector("#foto").src= images[i];
+    if (i < images.length -1){
+        i++;
+    } else {
+        i=0;
+    }
     
+  
+
+setTimeout("slideAuto()", time);
+   
 }
+window.onload = slideAuto();
