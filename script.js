ed4c2acfa81f8d3a7ba4a528b102bf5aa8e3eb04
mmmarceloo <83249188+mmmarceloo@users.noreply.github.com>
2021-07-13 16:15:49 -0300

    Add files via upload
    adicionei o zoom nas fotos, contato

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+function contato(){
+    var janela=document.getElementById("janelaModal-contato");
+    
+    janela.style.display="block";
+}
+function fechaContato(){
+    var janela=document.getElementById("janelaModal-contato");
+   
+   
+    janela.style.display="none";
+ 
+}
+
+
+
 var time = 3000;
    var i=0;
    var images = [];
@@ -31,3 +46,61 @@ setTimeout("slideAuto()", time);
    
 }
 window.onload = slideAuto();
+
+
+function clique(img){
+    var janela=document.getElementById("janelaModal");
+    var Btfecha=document.getElementById("fecha-janelaModal");
+    var imagem=document.getElementById("imagem-janelaModal");
+
+    janela.style.display="block";
+    imagem.src=img;
+    Btfecha.onclick=function(){
+        janela.style.display="none";
+        imagem.style.maxWidth="300px";
+        janela.style.paddingTop="10px";
+    }
+
+}
+function clique2(img){
+    var janela=document.getElementById("janelaModal");
+    var Btfecha=document.getElementById("fecha-janelaModal");
+    var imagem=document.getElementById("imagem-janelaModal");
+    
+    janela.style.display="block";
+    janela.style.paddingTop="100px";
+   
+    imagem.src=img;
+    imagem.style.maxWidth="800px";
+    Btfecha.onclick=function(){
+        janela.style.display="none";
+        imagem.style.maxWidth="300px";
+        janela.style.paddingTop="10px";
+    }
+
+}
+function clique3(img){
+    var janela=document.getElementById("janelaModal");
+    var Btfecha=document.getElementById("fecha-janelaModal");
+    var imagem=document.getElementById("imagem-janelaModal");
+    
+    janela.style.display="block";
+    janela.style.paddingTop="50px";
+    imagem.src=img;
+    imagem.style.maxWidth="600px";
+    Btfecha.onclick=function(){
+        janela.style.display="none";
+        imagem.style.maxWidth="300px";
+        janela.style.paddingTop="10px";
+    }
+
+}
+function fecha(){
+    var janela=document.getElementById("janelaModal");
+    var Btfecha=document.getElementById("fecha-janelaModal");
+    var imagem=document.getElementById("imagem-janelaModal");
+   
+    janela.style.display="none";
+    imagem.style.maxWidth="300px";
+    janela.style.paddingTop="10px";
+}
